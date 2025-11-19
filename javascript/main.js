be56e2ff513f35af41cd401cc85b40213a6e98ce
SuriyoVerweij <302376335@student.rocmondriaan.nl>
2025-11-19 18:15:15 +0100

    Game Data Prototype. Collision moet gefixed worden. "var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));" heeft een probleem.

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+console.log("Project Begonnen")
+
+var character = document.querySelector("#character");
+var block = document.querySelector("block");
+
+function jump(){
+    if (character.classList.contains("animate")){return}
+    
+    character.classList.add("animate");
+    setTimeout(() => {
+        character.classList.remove("animate")
+    }, 500);
+}
+
+var checkDead = setInterval(function () {
+    var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft =
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 &&
+        characterTop>=130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("Crash!")
+    }
+}, 10);
